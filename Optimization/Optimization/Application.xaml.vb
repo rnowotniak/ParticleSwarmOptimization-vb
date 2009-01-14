7fc4779515340e32c321fbfc92412b3c8f55b22f
Robert Nowotniak <robert@nowotniak.com>
2009-01-14 20:22:21 +0000

    Zrobiony parser wyrazen matematycznych
    git-svn-id: file:///home/rob/www/git/svn-old/VB@3 b48f1dc0-d380-11dd-a291-c53781e91fe5

--- Optimization/Optimization/Application.xaml.vb
+++ Optimization/Optimization/Application.xaml.vb
@@ -3,4 +3,11 @@
     ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
     ' can be handled in this file.
 
+    Public Sub New()
+        Dim mep As MathExpressionParser = New MathExpressionParser()
+        mep.init("x+yfsdf")
+        MsgBox(Str(mep.eval(9, 15)))
+        Environment.Exit(0)
+    End Sub
+
 End Class
---
+++ Optimization/Optimization/MathExpressionParser.vb
@@ -0,0 +1,46 @@
+﻿Public MustInherit Class ExpressionBaseClass
+    Public MustOverride Function eval(ByVal x As Double, ByVal y As Double) As Double
+End Class
+
+Public Class MathExpressionParser
+
+    Private expressionObject As ExpressionBaseClass
+
+    Public Sub init(ByVal expr As String)
+        Dim cp As Microsoft.CSharp.CSharpCodeProvider _
+            = New Microsoft.CSharp.CSharpCodeProvider()
+        Dim ic As System.CodeDom.Compiler.ICodeCompiler = cp.CreateCompiler()
+        Dim cpar As System.CodeDom.Compiler.CompilerParameters = _
+            New System.CodeDom.Compiler.CompilerParameters()
+        cpar.GenerateInMemory = True
+        cpar.GenerateExecutable = False
+        cpar.ReferencedAssemblies.Add("system.dll")
+        cpar.ReferencedAssemblies.Add("Optimization.exe")
+        Dim src = "using System;" & _
+            "class ExpressionClass : Optimization.ExpressionBaseClass {" & _
+            "  public override double eval(double x, double y) {" & _
+            "  return " & expr & " ;" & _
+            "  }" & _
+            "}"
+        Dim cr As System.CodeDom.Compiler.CompilerResults = _
+            ic.CompileAssemblyFromSource(cpar, src)
+        For Each ce As System.CodeDom.Compiler.CompilerError In cr.Errors
+            MsgBox(ce.ErrorText)
+        Next
+        If cr.Errors.Count = 0 Then
+            Dim o As Object = Activator.CreateInstance( _
+                cr.CompiledAssembly.GetType("ExpressionClass"))
+            expressionObject = o
+        Else
+            MsgBox("Wrong syntax in mathematical expression")
+        End If
+    End Sub
+
+    Public Function eval(ByVal x As Double, ByVal y As Double) As Double
+        If expressionObject Is Nothing Then
+            Throw New Exception("expression parser not initialised")
+        End If
+        Return expressionObject.eval(x, y)
+    End Function
+
+End Class
