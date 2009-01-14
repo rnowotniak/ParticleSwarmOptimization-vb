1bac2bc7f469f3e072e393f14646f97a4f57feda
Robert Nowotniak <robert@nowotniak.com>
2009-01-14 19:16:48 +0000

    Skończony projekt GUI
    git-svn-id: file:///home/rob/www/git/svn-old/VB@2 b48f1dc0-d380-11dd-a291-c53781e91fe5

---
+++ Optimization/Optimization/MainWindow.xaml.vb
@@ -0,0 +1,15 @@
+﻿Partial Public Class MainWindow
+
+    Public Sub New()
+        InitializeComponent()
+        debugTextBox.Text = "Application started" & vbCrLf
+    End Sub
+
+    Private Sub quitMenuItem_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles quitMenuItem.Click
+        Environment.Exit(0)
+    End Sub
+
+    Private Sub startButton_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles startButton.Click
+        debugTextBox.Text += "fasdf" & vbCrLf
+    End Sub
+End Class
---
+++ Optimization/Optimization/UserControl1.xaml.vb
@@ -0,0 +1,3 @@
+﻿Partial Public Class UserControl1
+
+End Class
